7ba3ddbddcab0c1de40c2283e987a46950650e77
+ <+>
2024-04-09 23:07:15 +0300

    create index.js

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const errorModal = document.querySelector('.error-modal');
+  errorModal.classList.add('hidden');
 
+  const emptyHearts = document.querySelectorAll('.empty-heart');
+  emptyHearts.forEach((heart) => {
+    heart.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          heart.classList.add('activated-heart');
+          heart.classList.remove('empty-heart');
+        })
+        .catch((error) => {
+          errorModal.classList.remove('hidden');
+          errorModal.querySelector('.error-message').textContent = error.message;
+          setTimeout(() => {
+            errorModal.classList.add('hidden');
+          }, 3000);
+        });
+    });
+  });
+
+  const fullHearts = document.querySelectorAll('.activated-heart');
+  fullHearts.forEach((heart) => {
+    heart.addEventListener('click', () => {
+      heart.classList.add('empty-heart');
+      heart.classList.remove('activated-heart');
+    });
+  });
+});
 
 
 
